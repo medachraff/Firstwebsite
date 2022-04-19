043901f9dbf05e146b85bf6323ae14f846f3ccb8
medachraff <achrafbensaad@icloud.com>
2022-04-19 23:50:48 +0100

    heyy

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Home = () => (
   <div className="home">
     <h1>Hello i am a website !</h1>
     <h1>Dedicace l gassara !</h1>
-    <h1>w a7la bac info hhh...</h1>
+    <h1>w a7la bac info hhh..aaa</h1>
 
     <div className="card-grid"></div>
   </div>
